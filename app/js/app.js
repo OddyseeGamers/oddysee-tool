cbe358696df05e764711a1b87c596ad253d784ed
Martin Skowronski <skowronski.martin@gmail.com>
2015-01-03 19:40:20 +0100

    Migrated tabs from jqueryui to bootstrap

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,24 @@
 $(document).ready(function () {
+
+    
+    console.debug("start");
+    $('#myTab a:first').tab('show');
+
+    initOrgStruc();
+    initOrg(d3.select("#svg"), orgstruc);
+    initWidgets();
+    initAssign();
+    initMemberList(mems);
+
+
+//     $('#myTab a').click(function (e) {
+//         e.preventDefault();
+//         console.debug("clicked");
+//         $(this).tab('hide');
+//     });
+//     $('#myTab a:first').tab('show');
+
+    /*
     $( "#tabs" ).tabs();
     initOrgStruc();
     initOrg(d3.select("#svg"), orgstruc);
@@ -6,4 +26,5 @@ $(document).ready(function () {
 
     initAssign();
     initMemberList(mems);
+    */
 });
