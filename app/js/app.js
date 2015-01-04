55caeb56cabeffcdb6989fdd74e9b985f9fa6794
Martin Skowronski <skowronski.martin@gmail.com>
2015-01-04 04:42:19 +0100

    minor changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {
-    $('#myTab a:first').tab('show');
+//     $('#myTab a:first').tab('show');
+    $('#myTab a[href="#mem_mgmt"]').tab('show');
     $('#myModal').modal({show: false, backdrop: "static"});
 
 //     $('#myDropdown').on('hide.bs.dropdown', function () {
 //         console.debug("OKOK", $('#myDropdown').dropdown());
 //     });
 
-//                                                 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-//                                                 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-//                                                 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-//                                                 <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
 
     initOrgStruc();
     initPath();
